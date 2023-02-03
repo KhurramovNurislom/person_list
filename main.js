fb8b5e0b7412ea2b72ca7dee0ff9019ab64ca80e
Nurislom <AlisherUzakov2406@gmail.com>
2023-02-03 12:02:35 +0500

    88

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log(35)
+
+
+const person_one = Object.create({}, {
+   //firstname : { value : 'Nurislom' },
+   lastname : { value : 'Khurramov'},
+   age: {value : 26}  
+});
+
+//console.log(firstname);
+console.log(lastname);
+console.log(age);
