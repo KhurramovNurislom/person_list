17a3d902adb5995aeae524bcb4bb16a433f3a59e
KhurramovNurislom <qwertyqwertyns0604@gmail.com>
2023-02-08 07:35:16 +0500

    asa

--- main.js
+++ main.js
@@ -1,12 +1,159 @@
-console.log(35)
+
+// Obektlar
+
+// Students
+console.log('Students object');
+
+const students = Object.create({},
+   {
+       firstName: {
+           value: 'Nurislom',
+           writable: true,
+           configurable: true,
+           enumerable: true
+       },
+
+       lastName: {
+           value: 'Xurramov'
+       },
+
+       age: {
+           value: 25
+       },
+
+       birtYear: {
+           get() {
+               return new Date().getFullYear() - this.age;
+           }
+       },
+
+       kunsi: {get(){
+         valuse : 4
+       }},
+
+       fakulteti : {
+         get(){
+            value : 'matematika'
+         }
+       },
 
 
-const person_one = Object.create({}, {
-   //firstname : { value : 'Nurislom' },
-   lastname : { value : 'Khurramov'},
-   age: {value : 26}  
-});
+       phoneNumber : {
+         get() {
+            value: '+998 94 648 83 34'
+         }
+       }
 
-//console.log(firstname);
-console.log(lastname);
-console.log(age);
+   });
+
+
+
+
+
+
+
+
+// Employees
+console.log('Employees object')
+
+
+const employees = Object.create({},
+   
+   { configurable: true,
+      writable: true,
+      enumerable: true,
+ 
+       firstName: {
+           value: 'Nurislom',
+       },
+
+       lastName: {
+           value: 'Xurramov'
+       },
+
+       age: {
+           value: 25
+       },
+
+       birtYear: {
+           get() {
+               return new Date().getFullYear() - this.age;
+           }
+       },
+
+       job: {
+         get(){
+            value: 'injener'
+         }
+       },
+
+       staji: {
+         get() {
+            value: 3
+         }
+       },
+
+       phoneNumber : {
+         get() {
+            value: '+998 94 648 83 34'
+         }
+       }
+
+
+
+   });
+
+
+
+
+
+
+console.log('cars object')
+
+
+
+// Cars
+const cars = Object.create({},
+   {
+       marka: {
+           value: 'BMW',
+           writable: true,
+           configurable: true,
+           enumerable: true
+       },
+
+       model: {
+           value: 'X6'
+       },
+
+       madeYear: {
+           value: 2022,         
+       },
+
+       color: {
+         get(){
+            value: 'BLACK'
+       }
+      },
+
+       maxspeed: {
+           get() {
+               value: 250
+           }
+       },
+
+       horsepower : {
+         get (){
+value: 600
+         }
+       }
+
+   });
+
+
+
+
+
+
+
+
