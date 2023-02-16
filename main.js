0d15614e7cfd76f62d18a1fe14c95cef94c00963
KhurramovNurislom <AlisherUzakov2406@gmail.com>
2023-02-16 15:40:48 +0500

    16_02_2023 15_40

--- main.js
+++ main.js
@@ -1,325 +1,208 @@
 
-// // Obektlar
+const myDiv = document.getElementById('myDiv');
 
-// // Students
-// console.log('Students object');
+function qiymatOlish() {
 
-// const students = Object.create({},
-//    {
-//        firstName: {
-//            value: 'Nurislom',
-//            writable: true,
-//            configurable: true,
-//            enumerable: true
-//        },
 
-//        lastName: {
-//            value: 'Xurramov'
-//        },
 
-//        age: {
-//            value: 25
-//        },
+}
 
-//        birtYear: {
-//            get() {
-//                return new Date().getFullYear() - this.age;
-//            }
-//        },
 
-//        kunsi: {get(){
-//          valuse : 4
-//        }},
 
-//        fakulteti : {
-//          get(){
-//             value : 'matematika'
-//          }
-//        },
+class Address {
+    constructor(viloyati, tumani, mahallasi, uyi) {
+        this.viloyati = viloyati;
+        this.tumani = tumani;
+        this.mahallasi = mahallasi;
+        this.uyi = uyi;
+    }
 
-
-//        phoneNumber : {
-//          get() {
-//             value: '+998 94 648 83 34'
-//          }
-//        }
-
-//    });
-
-
-
-
-
-
-
-
-// // Employees
-// console.log('Employees object')
-
-
-// const employees = Object.create({},
-   
-//    { configurable: true,
-//       writable: true,
-//       enumerable: true,
- 
-//        firstName: {
-//            value: 'Nurislom',
-//        },
-
-//        lastName: {
-//            value: 'Xurramov'
-//        },
-
-//        age: {
-//            value: 25
-//        },
-
-//        birtYear: {
-//            get() {
-//                return new Date().getFullYear() - this.age;
-//            }
-//        },
-
-//        job: {
-//          get(){
-//             value: 'injener'
-//          }
-//        },
-
-//        staji: {
-//          get() {
-//             value: 3
-//          }
-//        },
-
-//        phoneNumber : {
-//          get() {
-//             value: '+998 94 648 83 34'
-//          }
-//        }
-
-
-
-//    });
-
-
-
-
-
-
-// console.log('cars object')
-
-
-
-// // Cars
-// const cars = Object.create({},
-//    {
-//        marka: {
-//            value: 'BMW',
-//            writable: true,
-//            configurable: true,
-//            enumerable: true
-//        },
-
-//        model: {
-//            value: 'X6'
-//        },
-
-//        madeYear: {
-//            value: 2022,         
-//        },
-
-//        color: {
-//          get(){
-//             value: 'BLACK'
-//        }
-//       },
-
-//        maxspeed: {
-//            get() {
-//                value: 250
-//            }
-//        },
-
-//        horsepower : {
-//          get (){
-// value: 600
-//          }
-//        }
-
-//    });
-
-
-
-
-
-// //console.log('JS')
-
-// // Obektlar
-// // const person1 = Object.create({
-// //     fullName: function () {
-// //         return `${this.firstName} ${this.lastName}`
-// //     }
-// // },
-// //     {
-// //         firstName: {
-// //             value: 'Sobir',
-// //             writable: true,
-// //             configurable: true,
-// //             enumerable: true
-// //         },
-// //         lastName: {
-// //             value: 'Axmedov',
-// //             enumerable: true
-// //         },
-// //         age: {
-// //             value: 25,
-// //             enumerable: true
-// //         },
-// //         birtYear: {
-// //             get() {
-// //                 return new Date().getFullYear() - this.age;
-// //             }
-// //         }
-
-// //     });
-// // console.log(person1.fullName());
-// // for (const key in person1) {
-// //     console.log(key)
-// //     if (Object.hasOwnProperty.call(person1, key)) {
-// //         const element = person1[key];
-// //         console.log(element)
-// //     }
-// // }
-
-
-// class Person {
-//     constructor(firstName, lastName, age) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.age = age;
-//     }
-// }
-
-
-
-
-
-// const p2 = new Person('Axmedov', 'Ravshan', 30);
-// // console.log(p2)
-
-// class Doctor extends Person {
-//     constructor(firstName, lastName, age, spes, addess) {
-//         super(firstName, lastName, age);
-//         this.mutaxassisligi = spes;
-//         this.addess = addess;
-//     }
-// }
-// const d1 = new Doctor('Axmedov', 'Ravshan', 30, 'ВОП', { city: 'Toshkent' });
-// let d2 = {};
-// let d3 = d1;
-// Object.assign(d2, d1)
-// console.log(d1)
-// d2.age = 31
-// d2.addess.city = 'samarqand'
-// d1.age = 33;
-// console.log(d2)
-// console.log(d3)
-// //JSON
-// const d2_string = JSON.stringify(d2);
-// const d2_json = JSON.parse(d2_string);
-// d2_json.addess.city = 'andijon'
-// console.log(d2_string)
-// console.log(d2_json)
-// Massivlar
-
-// const persons = new Array();
-
-// persons[0]='Olim'
-// console.log(persons[0])
-// persons[3]='Olim'
-// console.log(persons);
-// const persons_2 = ['Olim', 'Ravshan', 'Maxmud', 'Tolib'];
-
-
-// //push
-// persons_2.push(new Person('Mamurov', 'Sherali', 26))//massivning oxiridan qushadi
-// persons_2.unshift(new Person('Juraqulov', 'Jamshid', 26))//massivning boshidan element qushadi
-// // persons_2.pop()
-// // persons_2.pop()
-// const element = persons_2.at(-1);//oxirgi elementini olish
-// const element_last = persons_2[persons_2.length - 1]//oxirgi elementini olish
-// console.log(element)
-// console.log(persons_2)
-// for (const element of persons_2) {
-//     console.log(element)
-// }
-// //rest
-// const countries_euro = ['Angliya', 'Italiya'];
-// const countries_osiyo = ['Uzbekiston', 'Kazakistan'];
-// const all_countries = [...countries_euro, ...countries_osiyo]
-// console.log(all_countries)
-// //object
-// const p1 = { firtsName: 'sas' }
-// const p2 = { ...p1, lastName: 'gggg' }
-// console.log(p2)
-
-
-// /********  Misol ************/
-// function Car(make, model, year) {
-//     this.make = make;
-//     this.model = model;
-//     this.year = year;
-//   }
-
-
-
-//   const car1 = new Car('USA', 'BMD', 2022)
-  
-// //   console.log(car1)
-
-// const car2 = new  Car('UK', 'Mers', 2020)
-// //   console.log(car2.make)
-
-
-//   function Employees(firstName, surName, Car){
-//     this.firstName = firstName
-//     this.surName = surName
-//     this.Car = Car
-//   }
-
-
-// const emp1 = new Employees('Nurislom', 'Xurramov', car1)
-// console.log(emp1)
-
-
-
-
-
+}
 
 class Person {
-    constructor(firstName, lastName, age, phoneNumber, timeOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.phoneNumber;
-        this.timeOfBirth = timeOfBirth;
+    constructor(ismi, familiyasi, sharifi, tugilgan_sanasi, unvoni, lavozimi, tel_nomeri, address) {
+        this.ismi = ismi;
+        this.familiyasi = familiyasi;
+        this.sharifi = sharifi;
+        this.tugilgan_sanasi = tugilgan_sanasi;
+        this.unvoni = unvoni;
+        this.lavozimi = lavozimi;
+        this.tel_nomeri = tel_nomeri;
+        this.address = address;
     }
 }
 
-const person = new Person('Nurislom', 'Xurramov', 25, '+998 94 648 83 34', '24.06.1995')
 
-class Employees extends Person{
-constructor(person, position)
-{
-    super(person)
-    this.position = position
-}
-}
 
-const emp1 = new Employees(person, 'Engener')
-console.log(emp1)
+let persons = []
 
 
 
+
+const btnIzlash = document.getElementById('btnIzlash');
+btnIzlash.addEventListener('click', (e) => {
+    const myForm = document.forms.myForm;
+    const izlanayotganIsm = myForm.elements.Izlash.value
+
+    console.log(izlanayotganIsm)
+
+    const persons_from_local = localStorage.getItem('persons')
+    if (persons_from_local != null) {
+        console.log('JSON parse')
+        const json = JSON.parse(persons_from_local)
+        persons = [...json]
+        console.log(persons)
+    }
+    else {
+        console.log('Data not find')
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+        if (izlanayotganIsm === persons[i].ismi) {
+            console.log(persons[i])
+
+        }
+    }
+})
+
+
+const btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click', (e) => {
+    const myForm = document.forms.myForm;
+    persons.push(new Person(
+        myForm.elements.ismi.value,
+        myForm.elements.familiyasi.value,
+        myForm.elements.sharifi.value,
+        myForm.elements.tugilgan_sanasi.value,
+        myForm.elements.unvoni.value,
+        myForm.elements.lavozimi.value,
+        myForm.elements.tel_nomeri.value, new Address(
+            myForm.elements.viloyati.value, myForm.elements.tumani.value,
+            myForm.elements.mahallasi.value, myForm.elements.uyi.value)))
+
+    localStorage.setItem('persons', JSON.stringify(persons))
+
+    const persons_from_local = localStorage.getItem('persons')
+    console.log(persons_from_local)
+
+    if (persons_from_local != null) {
+        console.log('JSON parse')
+        const json = JSON.parse(persons_from_local)
+        persons = [...json]
+        console.log(persons)
+    }
+    else {
+        console.log('Data not find')
+    }
+
+});
+
+
+const btnDelete = document.getElementById('btnDelete');
+btnDelete.addEventListener('click', (e) => {
+    const myForm = document.forms.myForm;
+
+    const persons_from_local = localStorage.getItem('persons')
+    // console.log(persons_from_local)
+
+    if (persons_from_local != null) {
+        console.log('JSON parse')
+        const json = JSON.parse(persons_from_local)
+        persons = [...json]
+        // console.log(persons)
+    }
+    else {
+        console.log('Data not find')
+    }
+
+    console.log(persons)
+
+    for (let i = 0; i < persons_from_local.length; i++) {
+        if (myForm.elements.Izlash.value === persons_from_local[i].ismi) {
+            persons.splice(i, 1);
+        }
+    }
+
+    console.log(persons)
+    // localStorage.setItem('persons', JSON.stringify(persons))
+});
+
+
+
+const btnEdit = document.getElementById('btnEdit');
+btnEdit.addEventListener('click', (e) => {
+    const myForm = document.forms.myForm;
+
+    const persons_from_local = localStorage.getItem('persons')
+    console.log(persons_from_local)
+
+    if (persons_from_local != null) {
+        console.log('JSON parse')
+        const json = JSON.parse(persons_from_local)
+        persons = [...json]
+        console.log(persons)
+    }
+    else {
+        console.log('Data not find')
+    }
+
+
+
+    const person = new Person(
+        myForm.elements.ismi.value,
+        myForm.elements.familiyasi.value,
+        myForm.elements.sharifi.value,
+        myForm.elements.tugilgan_sanasi.value,
+        myForm.elements.unvoni.value,
+        myForm.elements.lavozimi.value,
+        myForm.elements.tel_nomeri.value, new Address(
+            myForm.elements.viloyati.value, myForm.elements.tumani.value,
+            myForm.elements.mahallasi.value, myForm.elements.uyi.value))
+
+
+    for (let i = 0; i < persons_from_local.length; i++) {
+        if (myForm.elements.ismi.value === persons_from_local[i].ismi) {
+
+            persons_from_local.splice().setItem(person)
+            localStorage.setItem('persons', JSON.stringify(persons_from_local))
+
+        }
+    }
+
+
+
+    const persons_from_local_new = localStorage.getItem('persons')
+    console.log(persons_from_local_new)
+
+    if (persons_from_local_new != null) {
+        console.log('JSON parse')
+        const json_new = JSON.parse(persons_from_local_new)
+        persons = [...json_new]
+        console.log(persons)
+        console.log("ishladi...")
+    }
+    else {
+        console.log('Data not find')
+    }
+
+});
+
+
+
+const btnShow = document.getElementById('btnShow');
+btnShow.addEventListener('click', (e) => {
+    const myForm = document.forms.myForm;
+
+    const persons_from_local = localStorage.getItem('persons')
+    console.log(persons_from_local)
+
+    if (persons_from_local != null) {
+        console.log('JSON parse')
+        const json = JSON.parse(persons_from_local)
+        persons = [...json]
+        console.log(persons)
+    }
+    else {
+        console.log('Data not find')
+    }
+});
